e3d49da434d36064c04146cb14ce729f5e89a65f
Валерия Попова <gyrylovav03@mail.ru>
2022-12-12 13:52:52 +0700

    Стихотворение

--- lab7.js
+++ lab7.js
@@ -64,3 +64,19 @@ function makeSnow() {
         snows [i].firstChild.style.width = size;
     }
 }
+function makePoet() {
+    let stih = document.getElementsByClassName('poet');
+    for (let i = 0; i<stih.length; i++) {
+        stih [i].style.fontSize = (17 + (i+1)) + 'px';
+        let r = Math.round(Math.random()*255);
+        let g = Math.round(Math.random()*255);
+        let b = Math.round(Math.random()*255);
+        let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        stih [i].style.color = color;
+        r = Math.round(Math.random()*255);
+        g = Math.round(Math.random()*255);
+        b = Math.round(Math.random()*255);
+        color = 'rgb(' + r + ',' + g + ',' + b + ')';
+        stih [i].style.backgroundColor = color;
+    }
+}
